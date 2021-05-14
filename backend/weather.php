ec5ececcad56a400283956f3cef33b66e026ce1d
Surycx <83983725+Surycx@users.noreply.github.com>
2021-05-14 11:26:03 +0200

    deleted $current
    with OneCall API from OWM you get current weather from just one request

--- backend/weather.php
+++ backend/weather.php
@@ -3,12 +3,10 @@
 
 	header( 'Content-Type: application/json' );
 
-	$current  = file_get_contents( 'https://api.openweathermap.org/data/2.5/weather?id=' . OWM_CITY_ID . '&appid=' . OWM_API_KEY . '&units=' . OWM_UNITS );
 	$forecast = file_get_contents( 'https://api.openweathermap.org/data/2.5/onecall?lat=' . OWM_LAT . '&lon=' . OWM_LONG . '&exclude=' . OWM_EXCLUDE . '&appid=' . OWM_API_KEY);	
 
 	$data = new stdClass();
 	
-	$data->current = json_decode( $current );
 	$data->forecast = json_decode( $forecast );
 	$data->tempUnit = TEMP_UNIT;
 	$data->forecastItems = FORECAST_ITEMS;
