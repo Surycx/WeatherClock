5dac7ad26535680b2451a423e314f66b2da6c66c
Surycx <83983725+Surycx@users.noreply.github.com>
2021-05-14 11:25:13 +0200

    added some OWM item
    added OWM items for the use of OneCall API

--- backend/config-dist.php
+++ backend/config-dist.php
@@ -3,10 +3,12 @@
 // edit and rename to config.php
 
 define( 'OWM_API_KEY', 		'enter your API key here' );
-define( 'OWM_CITY_ID',		'enter the ID of the city you want to fetch the weather forecast for' );
+define( 'OWM_LONG', 		'enter the longitude of the city you want to fetch the weather forecast for' );
+define( 'OWM_LAT', 		'enter the latitude of the city you want to fetch the weather forecast for' );
+define( 'OWM_EXCLUDE' , 	'enter the excludes you want' ); //see https://openweathermap.org/api/one-call-api 
 define( 'OWM_UNITS',		'metric' ); // see https://openweathermap.org/current#data
 define( 'TEMP_UNIT',		'°C' ); // if you change OWN_UNITS, you might want to edit this one too
-define( 'FORECAST_ITEMS',	9 ); // how many forecast items to show. You might need to change the css if you add too many
+define( 'FORECAST_ITEMS',	7); // how many forecast items to show. You might need to change the css if you add too many
 
 /* 
 	Uncomment this section if you wish to monitor what's playing on a Subsonic server
